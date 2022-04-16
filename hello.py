053a6a5bf0d9a4823dcfc29ca3d812159adf5146
mutinta <justinmutinta@hotmail.com>
2022-04-15 22:46:19 -0500

    Hello <user> and browser id

--- hello.py
+++ hello.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return '<h1>Hello World!</h>'
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, %s!</h1>' % name
+
+@app.route('/user-agent')
+def user_agent():
+    user_agent = request.headers.get('User-Agent')
+    return '<p>Your browser is %s</p>' % user_agent
 
 if __name__ == '__main__':
     app.run(debug=True)
